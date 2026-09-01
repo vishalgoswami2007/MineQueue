bb858be87db8bc919574a34cf6ad3b16dc3302c0
Vishal Goswami <vg0767875@gmail.com>
2026-09-01 20:03:44 +0530

    Add paymentRoute in Sever.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import AuthRoutes from "./src/routes/authRoutes.js";
 import dotenv from 'dotenv';
-
+import PaymentRoute from "../backend/src/routes/paymentRoutes.js"
 
 dotenv.config();
 
@@ -12,9 +12,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 app.use('/api/auth' , AuthRoutes);
-app.get('/', (req, res) => {
-   res.send('Server is working!');
-});
+app.use('/api/payment' , PaymentRoute);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("Mongoose Connect "))
