7cb443c752ba63cdee2103b19184efcfc5bd4f84
Vishal Goswami <vg0767875@gmail.com>
2026-09-07 18:36:21 +0530

    refactor: improve doctor dashboard layout

--- frontend/src/pages/doctor/DoctorDashboard.jsx
+++ frontend/src/pages/doctor/DoctorDashboard.jsx
@@ -1,13 +1,14 @@
-import { Outlet } from 'react-router-dom';
-import DoctorSidebar from '../../components/common/DoctorSidebar';
+import { Outlet } from "react-router-dom";
+import DoctorSidebar from "../../components/common/DoctorSidebar";
 
 function DoctorDashboard() {
   return (
-    <div className="min-h-screen flex bg-gray-50">
+    <div className="flex min-h-screen bg-gray-50">
       <DoctorSidebar />
-      <div className="flex-1 p-8">
+
+      <main className="min-w-0 flex-1 overflow-x-hidden p-4 sm:p-6 lg:p-8">
         <Outlet />
-      </div>
+      </main>
     </div>
   );
 }
