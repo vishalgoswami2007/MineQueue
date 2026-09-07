5c2701af6b37f55f5aa0b5010a2eeb08e3b8571d
Vishal Goswami <vg0767875@gmail.com>
2026-09-07 20:18:10 +0530

    refactor: improve axios authentication handling

--- frontend/src/utils/AxiosInstance.js
+++ frontend/src/utils/AxiosInstance.js
@@ -1,15 +1,50 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-   baseURL: 'http://localhost:3000/api'
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:3000/api",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
+
+  timeout: 15000,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-   const token = localStorage.getItem('token');
-   if (token) {
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
       config.headers.Authorization = `Bearer ${token}`;
-   }
-   return config;
-});
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    const status = error.response?.status;
+
+    const token = localStorage.getItem("token");
+
+    if (status === 401 && token) {
+      localStorage.removeItem("token");
+
+      if (window.location.pathname !== "/logIn") {
+        window.location.href = "/logIn";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
