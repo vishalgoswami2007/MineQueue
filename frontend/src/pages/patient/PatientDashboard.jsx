5cd606cae0f52e6681f4208674cf0cc1c6061512
Vishal Goswami <vg0767875@gmail.com>
2026-09-07 20:36:46 +0530

    refactor: improve patient dashboard dark mode layout

--- frontend/src/pages/patient/PatientDashboard.jsx
+++ frontend/src/pages/patient/PatientDashboard.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/common/Sidebar";
 
 function PatientDashboard() {
   return (
-    <div className="flex min-h-screen bg-gray-50">
+    <div className="flex min-h-screen bg-gray-50 dark:bg-gray-950">
       <Sidebar />
 
       <main className="min-w-0 flex-1 overflow-x-hidden p-4 sm:p-6 lg:p-8">
